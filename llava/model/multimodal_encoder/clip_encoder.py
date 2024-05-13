9e8f75401110178a409e6d9715dc494550476a54
OliverSavolainen <oliverssavolainen@gmail.com>
2024-05-13 19:03:45 +0200

    Update clip_encoder.py

--- llava/model/multimodal_encoder/clip_encoder.py
+++ llava/model/multimodal_encoder/clip_encoder.py
@@ -21,7 +21,7 @@ class CLIPVisionTower(nn.Module):
         else:
             self.cfg_only = CLIPVisionConfig.from_pretrained(self.vision_tower_name)
 
-    def load_model(self, device_map=None):
+    def load_model(self, device_map="auto"):
         if self.is_loaded:
             print('{} is already loaded, `load_model` called again, skipping.'.format(self.vision_tower_name))
             return
